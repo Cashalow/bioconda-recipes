c7afaa7287d37da27435c286a2fc622d319bd388
Cashalow <sachalau@gmail.com>
2017-10-10 18:47:36 +0200

    adding blast 2.7

--- recipes/blast/build.sh
+++ recipes/blast/build.sh
@@ -2,25 +2,53 @@
 
 set -e -x -o pipefail
 
-mkdir -p $PREFIX/bin
+cd $SRC_DIR/c++/
 
-if [ "$(uname)" == "Darwin" ]; then
-    echo "Platform: Mac"
+export CFLAGS="$CFLAGS -O2"
+export CXXFLAGS="$CXXFLAGS -O2"
+export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
-    cp $SRC_DIR/bin/* $PREFIX/bin/
-
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "Platform: Linux"
-
-    cd $SRC_DIR/c++/
-    # --with-hard-runpath is needed otherwise BLAST programs would search
-    # libraries first in the directories defined by the LD_LIBRARY_PATH
-    # environment variable, instead of using the rpath specified by conda
-    ./configure --prefix=$PREFIX --with-hard-runpath
-
-    make -j${CPU_COUNT}
-
-    cp $SRC_DIR/c++/ReleaseMT/bin/* $PREFIX/bin/
+if test x"`uname`" = x"Linux"; then
+    # only add things needed; not supported by OSX ld
+    LDFLAGS="$LDFLAGS -Wl,-as-needed"
 fi
+
+# --with-hard-runpath is needed otherwise BLAST programs would search
+# libraries first in the directories defined by the LD_LIBRARY_PATH
+# environment variable, instead of using the rpath specified by conda
+
+./configure \
+    --with-dll \
+    --with-mt \
+    --without-autodep \
+    --without-makefile-auto-update \
+    --with-flat-makefile \
+    --without-caution \
+    --without-dbapi \
+    --without-lzo \
+    --with-hard-runpath \
+    --without-debug \
+    --with-strip \
+    --without-vdb \
+    --with-z=$PREFIX \
+    --with-bz2=$PREFIX \
+    --with-boost=$PREFIX \
+    --without-openssl \
+    --without-gcrypt \
+    --with-gnutls=$PREFIX \
+    --with-nettle=$PREFIX \
+    --without-krb5
+
+projects="algo/blast/ app/ objmgr/ objtools/align_format/ objtools/blast/"
+
+cd */build
+
+make -j${CPU_COUNT} -f Makefile.flat all_projects="$projects"
+
+mkdir -p $PREFIX/{bin,lib,include}
+rm $SRC_DIR/c++/ReleaseMT/bin/*_unit_test
+cp $SRC_DIR/c++/ReleaseMT/bin/* $PREFIX/bin/
+cp $SRC_DIR/c++/ReleaseMT/lib/* $PREFIX/lib/
 
 chmod +x $PREFIX/bin/*
